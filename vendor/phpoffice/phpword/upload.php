b7270f88c4fb5bd72c63eeabca289427eaff631f
user <xlnt83@gmail.com>
2019-01-12 22:06:37 +0200

    Update code

--- vendor/phpoffice/phpword/upload.php
+++ vendor/phpoffice/phpword/upload.php
@@ -17,6 +17,8 @@ echo '<html>
   </style>
 </header>
 <body>';
+
+
 require_once 'bootstrap.php';
 
 $target_dir = "uploads\\";
@@ -33,42 +35,45 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "Sorry, there was an error uploading your file.";
 }
 
-$html = file_get_html('http://test.local/uploads/TA.html');
+$html = file_get_html('http://' . $_SERVER['HTTP_HOST'] . '/uploads/TA.html');
 
 ?>
     <p align="center">
     <p align="center">
         <label><strong>Arotrede</strong></label></br>
         <textarea rows="10" cols="150">
-                     <?php echo get_done_html($html) ?>
+                     <?php echo trim(get_done_html($html)) ?>
             </textarea>
     </p>
     <p align="center">
         <label><strong>Opteck</strong></label></br>
         <textarea rows="10" cols="150">
-                      <?php echo get_done_html_opteck($html) ?>
+                      <?php echo trim(get_done_html_opteck($html)) ?>
             </textarea>
     </p>
 <?php
-$i =1;
-foreach (get_tables($html) as $table) {
+$i = 1;
+$table_libtaly = get_tables($html);
+if ($table_libtaly) {
+    foreach ($table_libtaly as $table) {
 
-    $table = str_replace('TIME FRAME', 'ПЕРИОД', $table);
-    $table = str_replace('PIVOT POINT', 'ОПОРНАЯ ТОЧКА', $table);
-    $table = str_replace('DAILY', '1 день', $table);
-    $table = str_replace('WEEKLY', '1 неделя', $table);
-    $table = str_replace('MONTHLY', '1 месяц', $table);
-    ?>
-    <p align="center">
-        <label><strong>RU Table <?php echo $i?></strong></label></br>
-        <textarea rows="10" cols="150">
+        $table = str_replace('TIME FRAME', 'ПЕРИОД', $table);
+        $table = str_replace('PIVOT POINT', 'ОПОРНАЯ ТОЧКА', $table);
+        $table = str_replace('DAILY', '1 день', $table);
+        $table = str_replace('WEEKLY', '1 неделя', $table);
+        $table = str_replace('MONTHLY', '1 месяц', $table);
+        ?>
+        <p align="center">
+            <label><strong>RU Table <?php echo $i ?></strong></label></br>
+            <textarea rows="10" cols="150">
         <?php
         echo $table;
         ?>
         </textarea>
-    </p>
-    <?php
-    $i++;
+        </p>
+        <?php
+        $i++;
+    }
 }
 ?>
     </p>
@@ -97,42 +102,42 @@ function get_tables($html)
             $rowData[] = $flight;
         }
         $new_table = '<table border="1" cellpadding="1" cellspacing="1" style="width: 100%;">
-  <thead>
-  <tr>
-  <th scope="row">TIME FRAME</th>
-  <th scope="col">S3</th>
-  <th scope="col">S2</th>
-  <th scope="col">S1</th>
-  <th scope="col">PIVOT POINT</th>
-  <th scope="col">R1</th>
-  <th scope="col">R2</th>
-  <th scope="col">R3</th>
-  </tr>
-  </thead>
-  <tbody>
+      <thead>
+          <tr>
+              <th scope="row">TIME FRAME</th>
+              <th scope="col">S3</th>
+              <th scope="col">S2</th>
+              <th scope="col">S1</th>
+              <th scope="col">PIVOT POINT</th>
+              <th scope="col">R1</th>
+              <th scope="col">R2</th>
+              <th scope="col">R3</th>
+          </tr>
+      </thead>
+      <tbody>
   ';
 
         $numder_road;
         foreach ($rowData as $row => $tr) {
             if ($numder_road >= 1) {
-                $new_table .= '<tr>
+                $new_table .= '    <tr>
   ';
                 $i;
                 foreach ($tr as $td) {
                     if ($i == 0) {
-                        $new_table .= '<th scope="row">' . $td . '</th>
+                        $new_table .= '        <th scope="row">' . $td . '</th>
   ';
                     } elseif ($i == 4) {
-                        $new_table .= '<td><strong>' . $td . '</strong></td>
+                        $new_table .= '        <td><strong>' . $td . '</strong></td>
   ';
                     } else {
-                        $new_table .= '<td>' . $td . '</td>
+                        $new_table .= '        <td>' . $td . '</td>
   ';
                     }
                     $i++;
                 }
                 unset($i);
-                $new_table .= '</tr>
+                $new_table .= '    </tr>
   ';
             }
             $numder_road++;
@@ -140,8 +145,7 @@ function get_tables($html)
         unset($numder_road);
         $new_table .= '</tbody>
   </table>
-
-  ';
+ ';
         $table_libtaly[] = $new_table;
     }
     return $table_libtaly;
@@ -149,11 +153,12 @@ function get_tables($html)
 
 function get_done_html($html)
 {
+    $year_mons = date("Y/m");
     $yesterday = date("d-m-Y");
-    $img[] = '<img class="aligncenter size-full wp-image-7421" src="https://www.arotrade.com/wp-content/uploads/2019/01/USDJPY-' . $yesterday . '.png" alt="USDJPY-' . $yesterday . '" width="1640" height="867" />​';
-    $img[] = '<img class="aligncenter size-full wp-image-7422" src="https://www.arotrade.com/wp-content/uploads/2019/01/EURUSD-' . $yesterday . '.png" alt="EURUSD-' . $yesterday . '" width="1654" height="866" />​';
-    $img[] = '<img class="aligncenter size-full wp-image-7423" src="https://www.arotrade.com/wp-content/uploads/2019/01/GBPUSD-' . $yesterday . '.png" alt="GBPUSD-' . $yesterday . '" width="1653" height="861" />​';
-    $img[] = '<img class="aligncenter size-full wp-image-7424" src="https://www.arotrade.com/wp-content/uploads/2019/01/Gold-' . $yesterday . '.png" alt="Gold-' . $yesterday . '" width="1654" height="861" />​';
+    $img[] = '<img class="aligncenter size-full wp-image-7421" src="/wp-content/uploads/'.$year_mons.'/USDJPY-' . $yesterday . '.png" alt="USDJPY-' . $yesterday . '" width="1640" height="867" />​';
+    $img[] = '<img class="aligncenter size-full wp-image-7422" src="/wp-content/uploads/'.$year_mons.'/EURUSD-' . $yesterday . '.png" alt="EURUSD-' . $yesterday . '" width="1654" height="866" />​';
+    $img[] = '<img class="aligncenter size-full wp-image-7423" src="/wp-content/uploads/'.$year_mons.'/GBPUSD-' . $yesterday . '.png" alt="GBPUSD-' . $yesterday . '" width="1653" height="861" />​';
+    $img[] = '<img class="aligncenter size-full wp-image-7424" src="/wp-content/uploads/'.$year_mons.'/Gold-' . $yesterday . '.png" alt="Gold-' . $yesterday . '" width="1654" height="861" />​';
 
     $button[] = '<div class="pad-space center-align">[ts_is_trader_guest]<a href="/open-account/" class="btn middle-news invert">Trade USDJPY</a>[/ts_is_trader_guest][ts_is_trader_with_deposit]<a href="//trade.arotrade.com/cfd-tab/33" class="btn middle-news invert">Trade USDJPY</a>[/ts_is_trader_with_deposit][ts_is_trader_without_deposit]<a href="//trade.arotrade.com/deposit-funds/" class="btn middle-news invert">Trade USDJPY</a>[/ts_is_trader_without_deposit]</div>';
     $button[] = '<div class="pad-space center-align">[ts_is_trader_guest]<a href="/open-account/" class="btn middle-news invert">Trade EURUSD</a>[/ts_is_trader_guest][ts_is_trader_with_deposit]<a href="//trade.arotrade.com/cfd-tab/17" class="btn middle-news invert">Trade EURUSD</a>[/ts_is_trader_with_deposit][ts_is_trader_without_deposit]<a href="//trade.arotrade.com/deposit-funds/" class="btn middle-news invert">Trade EURUSD</a>[/ts_is_trader_without_deposit]</div>';
@@ -162,55 +167,62 @@ function get_done_html($html)
 
     $table_libtaly = get_tables($html);
     $i = 0;
-    foreach ($table_libtaly as $table) {
-        $team = '
-  ';
-        $team .= $img[$i];
-        $team .= '
-  ';
-        $team .= $table;
-        $team .= '
-  ';
-        $team .= $button[$i];
-        $team .= '
-  ';
-        $insert_puth[] = $team;
-        $i++;
-    }
+
     $my_all_taxt = text_preparation($html);
-    foreach ($insert_puth as $value) {
-        $my_all_taxt = str_replace_once("<h2>table</h2>", $value, $my_all_taxt);
+    if ($table_libtaly) {
+        foreach ($table_libtaly as $table) {
+            $team = '
+      ';
+            $team .= $img[$i];
+            $team .= '
+      ';
+            $team .= $table;
+            $team .= '
+      ';
+            $team .= $button[$i];
+            $team .= '
+      ';
+            $insert_puth[] = $team;
+            $i++;
+        }
+        foreach ($insert_puth as $value) {
+            $my_all_taxt = str_replace_once("<h2>table</h2>", $value, $my_all_taxt);
+        }
     }
     return $my_all_taxt;
 }
 
 function get_done_html_opteck($html)
 {
-    $yesterday = date("d-m-Y");
-    $img[] = ' <img src="//assets.opteck.com/attachments/blog/USDJPY-' . $yesterday . '.png" alt="USDJPY-' . $yesterday . '"/>​';
-    $img[] = '<img src="//assets.opteck.com/attachments/blog/EURUSD-' . $yesterday . '.png" alt="EURUSD-' . $yesterday . '" />​';
-    $img[] = ' <img src="//assets.opteck.com/attachments/blog/GBPUSD-' . $yesterday . '.png" alt="GBPUSD-' . $yesterday . '" />​';
-    $img[] = ' <img src="//assets.opteck.com/attachments/blog/Gold-' . $yesterday . '.png" alt="Gold-' . $yesterday . '" />​';
-
     $table_libtaly = get_tables($html);
-    $i = 0;
-    foreach ($table_libtaly as $table) {
-        $team = '
-  ';
-        $team .= $img[$i];
-        $team .= '
-            <p>&nbsp;</p>
-  ';
-        $team .= $table;
-        $team .= '
-            <p>&nbsp;</p> 
-  ';
-        $insert_puth[] = $team;
-        $i++;
-    }
     $my_all_taxt = text_preparation($html);
-    foreach ($insert_puth as $value) {
-        $my_all_taxt = str_replace_once("<h2>table</h2>", $value, $my_all_taxt);
+    if ($table_libtaly) {
+        $yesterday = date("d-m-Y");
+        $img[] = ' <img src="//assets.opteck.com/attachments/blog/USDJPY-' . $yesterday . '.png" alt="USDJPY-' . $yesterday . '"/>​';
+        $img[] = ' <img src="//assets.opteck.com/attachments/blog/EURUSD-' . $yesterday . '.png" alt="EURUSD-' . $yesterday . '" />​';
+        $img[] = ' <img src="//assets.opteck.com/attachments/blog/GBPUSD-' . $yesterday . '.png" alt="GBPUSD-' . $yesterday . '" />​';
+        $img[] = ' <img src="//assets.opteck.com/attachments/blog/Gold-' . $yesterday . '.png" alt="Gold-' . $yesterday . '" />​';
+
+        $i = 0;
+        foreach ($table_libtaly as $table) {
+            $team = '
+  ';
+            $team .= $img[$i];
+            $team .= '
+            <p>'.htmlspecialchars("&nbsp;", ENT_QUOTES).'</p>
+  ';
+            $team .= $table;
+            $team .= '
+
+            <p>'.htmlspecialchars("&nbsp;", ENT_QUOTES).'</p>
+  ';
+            $insert_puth[] = $team;
+            $i++;
+        }
+
+        foreach ($insert_puth as $value) {
+            $my_all_taxt = str_replace_once("<h2>table</h2>", $value, $my_all_taxt);
+        }
     }
     return $my_all_taxt;
 }
